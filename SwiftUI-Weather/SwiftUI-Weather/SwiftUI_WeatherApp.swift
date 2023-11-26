17da6e21515068f643e7e4079ee4d3549e2c34fd
EddieZhan <nianyi234@gmail.com>
2023-11-26 13:14:37 +0800

    init

---
+++ SwiftUI-Weather/SwiftUI-Weather/SwiftUI_WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_WeatherApp.swift
+//  SwiftUI-Weather
+//
+//  Created by EddieZhan on 2023/11/26.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
