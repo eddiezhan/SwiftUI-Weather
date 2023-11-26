3c445c14825855cd73993d2453af4e9e84e9185a
EddieZhan <nianyi234@gmail.com>
2023-11-26 23:03:19 +0800

    添加 多日天气

--- SwiftUI-Weather/SwiftUI-Weather/ContentView.swift
+++ SwiftUI-Weather/SwiftUI-Weather/ContentView.swift
@@ -8,17 +8,63 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+  var body: some View {
+    ZStack {
+      LinearGradient(gradient: Gradient(colors: [.blue, Color("lightBlue")]),
+                     startPoint: .topLeading,
+                     endPoint: .bottomTrailing)
+        .edgesIgnoringSafeArea(.all)
+      VStack {
+        Text("无锡，江苏")
+          .font(.title).foregroundColor(.white)
+          .padding()
+
+        VStack(spacing: 10) {
+          Image(systemName: "cloud.sun.fill")
+            .renderingMode(.original)
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+            .frame(width: 180, height: 180)
+
+          Text("21℃")
+            .font(.largeTitle)
+            .foregroundColor(.white)
+        }.padding(.bottom, 40)
+
+        HStack(spacing: 20) {
+          WeatherDayView(dayOfWeek: "周一", imageName: "cloud.sun.fill", temperature: 26)
+          WeatherDayView(dayOfWeek: "周一", imageName: "cloud.sun.fill", temperature: 26)
+          WeatherDayView(dayOfWeek: "周一", imageName: "cloud.sun.fill", temperature: 26)
+          WeatherDayView(dayOfWeek: "周一", imageName: "cloud.sun.fill", temperature: 26)
+          WeatherDayView(dayOfWeek: "周一", imageName: "cloud.sun.fill", temperature: 26)
         }
-        .padding()
+        Spacer()
+      }
     }
+  }
 }
 
 #Preview {
-    ContentView()
+  ContentView()
+}
+
+struct WeatherDayView: View {
+  var dayOfWeek: String
+  var imageName: String
+  var temperature: Int
+
+  var body: some View {
+    VStack(spacing: 8) {
+      Text(dayOfWeek)
+        .font(.title2).foregroundColor(.white)
+      Image(systemName: imageName)
+        .renderingMode(.original)
+        .resizable()
+        .aspectRatio(contentMode: .fit)
+        .frame(width: 40, height: 40)
+      Text("\(temperature)")
+        .font(.title2)
+        .foregroundColor(.white)
+    }
+  }
 }
